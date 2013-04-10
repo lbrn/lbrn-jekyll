e8a8cf5478208324681a6ad718912737aa396248
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-04-10 13:31:01 -0500

    remove old hook

--- captainhook.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo shell_exec('cd ..; pwd');
-$output = shell_exec('cd ..; git pull');
-echo '<p>'.$output.'</p>';
-$output = shell_exec('cd ..; /var/lib/gems/1.8/bin/jekyll');
-echo '<p>'.$output.'</p>';
-?>
