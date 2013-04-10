6de905c4ffa38d0e312b415d301d88c8e9a09c58
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-04-10 12:33:38 -0500

    testing hook

--- captainhook.php
+++ captainhook.php
@@ -1,7 +1,6 @@
 <?php
-echo shell_exec('cd ..; ls');
 $output = shell_exec('cd ..; git pull');
-echo '<prev>'.$output.'</prev>';
+echo '<p>'.$output.'</p>';
 $output = shell_exec('cd ..; /var/lib/gems/1.8/bin/jekyll');
-echo '<prev>'.$output.'</prev>';
+echo '<p>'.$output.'</p>';
 ?>
